960d682ac2067891726fe536d88c0c11e5b517be
Ramadan Mohamed <elking.medo61111@gmail.com>
2025-09-02 01:08:05 +0300

    add effect on mobile view nad fix simple bug

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2804607171"
+    serviceWorkerVersion: "2359691440"
   }
 });
